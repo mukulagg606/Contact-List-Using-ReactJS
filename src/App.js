f05d475f1a1bc46bfb2af23f570b0c496fe362e9
Mukul AggArwal <115641523+mukulagg606@users.noreply.github.com>
2023-04-27 12:07:42 +0530

    created contact-list using react redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AddContact from "./components/AddContact";
+import EditContact from "./components/EditContact";
+import { useDispatch } from "react-redux";
+
+const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const data = [];
+        const promise = async () => {
+            await fetch('https://jsonplaceholder.typicode.com/users/')
+                .then((response) => response.json())
+                .then((json) => {
+                    json.map((contact) => {
+                        data.push({
+                            id: contact.id,
+                            name: contact.name,
+                            number: contact.phone,
+                            email: contact.email
+                        });
+                    })
+                });
+            dispatch({ type: 'FETCH_CONTACTS', payload: data });
+        };
+        promise();
+    }, [dispatch]);
+
+
+    return (
+        <div className="App">
+            <ToastContainer />
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />}>
+
+                </Route>
+                <Route path="/add" element={<AddContact />}>
+
+                </Route>
+                <Route path="/edit/:id" element={<EditContact />}>
+
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
